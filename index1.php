07462e154b1a188a6737f3bc23b0345200db70ae
Tom-Sarfo <74250845+Tom-Sarfo@users.noreply.github.com>
2023-06-24 22:02:03 +0000

    changes made to landing page

---
+++ index1.php
@@ -0,0 +1,184 @@
+ <?php 
+ ob_start();
+session_start();
+
+error_reporting(0);
+require_once 'db.php';
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    
+    <script async data-id="34503" src="https://cdn.widgetwhats.com/script.min.js"></script>
+    
+<title>Buy Quality Birk Slippers and Sandals in Ghana|TomusBirk</title>
+
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content="TomusBirk brings to you the quality and affordable birk slipper and sandals which comes in unique designs ">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--<link rel="icon" type="image/png" href="images/favicon-tomusbirk.png"/>-->
+<link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
+<link rel="manifest" href="/favicon/site.webmanifest">
+<link rel="stylesheet" type="text/css" href="styles/bootstrap-4.1.2/bootstrap.min.css">
+<link href="plugins/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.2.1/owl.carousel.css">
+<link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.2.1/owl.theme.default.css">
+<link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.2.1/animate.css">
+<link rel="stylesheet" type="text/css" href="styles/main_styles.css">
+<link rel="stylesheet" type="text/css" href="styles/responsive.css">
+<link rel="stylesheet" type="text/css" href="styles/loader.css">
+</head>
+<body onload="myFunction()" style="margin:0;">
+<div id="loader"></div>
+
+<!-- Menu -->
+
+<div style="display:none;" id="masterDiv" class="animate-bottom">
+<div class="menu">
+
+	<!-- Search -->
+	<!-- Search button goes here someday -->
+	<!-- Navigation -->
+	<div class="menu_nav" style="padding: -20px;">
+		<ul>
+		    <li>
+				<a href="#">
+				<?php
+				
+			    if (isset($_SESSION['user'])) {
+					echo $_SESSION['username'];
+				}
+				 
+				?>
+			 	
+			 </a>
+			 </li><span></span><br>
+			<li><a href="membership/login.php">Sign In</a></li>
+		</ul>
+	</div><span></span><br><br><br>
+	<div>
+	    <a href="membership/logout.php" type="button" class="btn btn-danger">Log Out</a>
+	</div>
+	<!-- Contact Info -->
+	
+	<div class="menu_contact">
+	    <span>Call us on: <a href="tel:+233549649770">+233549649770</a></span>
+		<div class="menu_phone d-flex flex-row align-items-center justify-content-start">
+			<!--<div><div><img src="images/phone.svg"></div></div>-->
+			
+			<!--<div><a href="tel:+233549649770"></a>+233549649770</a></div><span></span>-->
+			<!--<div><a href="tel:+233271000728">+233271000728</a></div>-->
+		</div>
+		<div class="menu_social">
+			<ul class="menu_social_list d-flex flex-row align-items-start justify-content-start">
+				<li><a href="https://web.facebook.com/tomusbirk/"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+				<li><a href="https://www.instagram.com/_tomusbirk/"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+			</ul>
+		</div>
+	</div>
+</div>
+
+<div class="super_container">
+
+	<!-- Header -->
+
+
+
+	<div class="flex row">
+	    Welcome to Tomusbirk 
+	</div>
+
+		<!-- Footer -->
+
+		<footer class="footer">
+			<div class="footer_content">
+				<div class="container">
+					<div class="row">
+						
+						<!-- About -->
+						<div class="col-lg-4 footer_col">
+								<div class="footer_about">
+								<div class="footer_logo">
+									<a href="#">
+										<div class="d-flex flex-row align-items-center justify-content-start">
+											<div class="footer_logo_icon"><a href="index.php"><img src="images/logo6.png" alt="TomusBirk Birk Store"></a></div>
+										</div>
+									</a>		
+								</div>
+								<div class="footer_about_text">
+									<p>Your feet deserve better!</p>
+								</div>
+							</div>
+						</div>
+
+						<!-- Footer Links -->
+					
+									<!--<li>-->
+									<!--	<a href="how_to_make_order.php"><div>How To Make An Order<div class="footer_tag_1">recommeded</div></div></a>-->
+
+						 <!--Footer Contact -->
+						<div class="col-lg-4 footer_col">
+							<div class="footer_contact">
+								<!--<div class="footer_title">Stay in Touch</div>-->
+								<!--<div class="newsletter">-->
+								<!--	<form action="#" id="newsletter_form" class="newsletter_form">-->
+								<!--		<input type="email" class="newsletter_input" placeholder="Subscribe to our Newsletter" required="required">-->
+								<!--		<button class="newsletter_button">+</button>-->
+								<!--	</form>-->
+								<!--</div>-->
+								<div class="footer_social">
+									<div class="footer_title">Connect with us on:</div>
+									<ul class="footer_social_list d-flex flex-row align-items-start justify-content-start">
+										<li><a href="https://web.facebook.com/tomusbirk/"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+										<li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+										<li><a href="https://www.instagram.com/_tomusbirk/"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="footer_bar">
+				<div class="container">
+					<div class="row">
+						<div class="col">
+							<div class="footer_bar_content d-flex flex-md-row flex-column align-items-center justify-content-start">
+								<div class="copyright order-md-1 order-2"><!-- TomusBirk has it now thank you-->
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | <a href="https://tomusbirk.com" target="_blank">TomusBirk.co</a>
+</div>
+								
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
+	</div>
+		
+</div>
+</div>
+<script src="js/jquery-3.2.1.min.js"></script>
+<script src="styles/bootstrap-4.1.2/popper.js"></script>
+<script src="styles/bootstrap-4.1.2/bootstrap.min.js"></script>
+<script src="plugins/greensock/TweenMax.min.js"></script>
+<script src="plugins/greensock/TimelineMax.min.js"></script>
+<script src="plugins/scrollmagic/ScrollMagic.min.js"></script>
+<script src="plugins/greensock/animation.gsap.min.js"></script>
+<script src="plugins/greensock/ScrollToPlugin.min.js"></script>
+<script src="plugins/OwlCarousel2-2.2.1/owl.carousel.js"></script>
+<script src="plugins/easing/easing.js"></script>
+<script src="plugins/progressbar/progressbar.min.js"></script>
+<script src="plugins/parallax-js-master/parallax.min.js"></script>
+<script src="js/custom.js"></script>
+<script src="js/owl-boxes.js"></script>
+<script src="js/script.js"></script>
+<script src="sweetalert/sweetalert.min.js"></script>
+
+</body>
+</html>
